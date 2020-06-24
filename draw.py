c8cd7519764a6defec0b382e26ab7bcb76e32642
Tina <tina.zhou.hui@gmail.com>
2020-06-24 22:07:55 +0200

    + penguin head and flips it

--- draw.py
+++ draw.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2
 
-image = np.zeros((512,512,3), np.uint8) # each zero has specific length, uint8 max 255
+penguins = cv2.imread('penguins.jpg', 0)
+
+image = np.zeros((512, 512, 3), np.uint8) # each zero has specific length, uint8 max 255
 # 3 cause RGB depth
 
 length_of_square = 100
@@ -16,8 +18,13 @@ for y in range(length_of_square):
     start_y += 1
 
 square = image[0:120, 0:120]
-
 image[120:240, 120:240] = square
 
+
+head = penguins[110:310, 750:970]
+head = np.flip(head, 1)
+penguins[0:200, 0:220] = head
+
 cv2.imwrite("image.jpg", image)
+cv2.imwrite("penguins_bnw.jpg", penguins)
 
