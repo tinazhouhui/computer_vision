ed3b2d67108a160e99bb8c50e3109c0a8517fad0
Tina <tina.zhou.hui@gmail.com>
2020-06-27 16:31:34 +0200

    + convolution principle

---
+++ convo.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+default = [
+    [10, 12, 14, 16, 18, 20, 22, 24, ],
+    [20, 22, 24, 26, 28, 30, 32, 34, ],
+    [30, 32, 34, 36, 38, 40, 42, 44, ],
+    [40, 42, 44, 46, 48, 50, 52, 54, ],
+    [50, 52, 54, 56, 58, 60, 62, 64, ],
+    [60, 62, 64, 66, 68, 70, 72, 74, ],
+]
+
+default = cv2.imread('input/sky.jpg', 0)
+
+constant = 1
+matrix = [
+    [-1, -1, -1, ],
+    [-1, 8, -1, ],
+    [-1, -1, -1, ],
+]
+
+
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        matrix[i][j] = constant * matrix[i][j]
+
+width = int(default.shape[1])
+height = int(default.shape[0])
+
+transformed = np.zeros((height, width))
+
+
+for y in range(1, len(default)-1):
+    for x in range(1, len(default[y])-1):
+
+        output_pixel = 0
+
+        for i in range(-1, len(matrix)-1):
+            for j in range(-1, len(matrix[i])-1):
+                output_pixel += default[y+j][x+i] * matrix[i+1][j+1]
+        transformed[y][x] = output_pixel
+
+cv2.imwrite('output/transformed.jpg', transformed)
+
+
+
+
