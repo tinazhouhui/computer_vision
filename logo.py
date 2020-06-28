951ec30883bd526f0490ceea790c92c79c0f8a78
Tina <tina.zhou.hui@gmail.com>
2020-06-28 17:57:07 +0200

    + opencv thresholds and bitwise

---
+++ logo.py
@@ -0,0 +1,33 @@
+"""
+excercise to work with bitwise operations.
+https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_core/py_image_arithmetics/py_image_arithmetics.html
+"""
+
+import cv2
+
+penguins = cv2.imread('input/penguins.jpg')
+logo = cv2.imread('input/codeac.png', 1)
+
+rows, cols, channels = logo.shape
+
+roi = penguins[0:rows, 0:cols]
+
+logo2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(logo2gray, 150, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+penguins_bg = cv2.bitwise_and(roi, roi, mask = mask)
+
+logo_fg = cv2.bitwise_and(logo, logo, mask = mask_inv)
+
+output = cv2.add(penguins_bg, logo_fg)
+penguins[0:rows, 0:cols] = output
+
+cv2.imwrite('output/logo_gray.jpg', mask)
+cv2.imwrite('output/penguins_bg.jpg', penguins_bg)
+cv2.imwrite('output/logo_fg.jpg', logo_fg)
+cv2.imwrite('output/penguins_logo.jpg', penguins)
+
+
+
+
