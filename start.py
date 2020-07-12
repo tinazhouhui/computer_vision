cfd031f68bccb0f2e326e5f9e07458084888eea1
Tina <tina.zhou.hui@gmail.com>
2020-07-12 21:37:14 +0200

    + call function router

--- start.py
+++ start.py
@@ -2,13 +2,13 @@ from sys import argv
 
 # list of all the possible transformations
 from image_processing.convo import create_transformed_outputs
+from router import router
 
-actions = {'draw': 'basic manipulation with images',
-           'blend': 'blending two images together (addWeighted)',
-           'bitwise-operations': 'bitwise operations',
+actions = {'blend': 'blending two images together (using addWeighted)',
+           'bitwise-operations': 'creates a logo that is inserted to a picture',
            'convolution': 'will run all available convolutions',
            'gamma-correction': 'gamma adjustment via linear stretching',
-           'coin-recognition': 'identify coins',
+           'coin-detection': 'compares outputs of manual and Hough detection of circles (coins)',
            }
 
 if len(argv) > 1: #checks for argument inputed in console
@@ -17,8 +17,8 @@ if len(argv) > 1: #checks for argument inputed in console
         for action, description in actions.items():
             print("\u2022\033[1m " + action + '\033[0m - ' + description)
 
-    if argv[1] == 'convolution':
-        create_transformed_outputs()
+    router(argv[1])()
+
 else:
     print('\nPlease add an argument behind "start.py" from the list below:\n')
     for action, description in actions.items():
